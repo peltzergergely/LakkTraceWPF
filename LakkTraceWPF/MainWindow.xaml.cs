26727bed6e758b90e3cc197951ce26e65ed17e46
peltzergergely <peltzer.gergely@gmail.com>
2017-12-29 18:00:07 +0100

    DB validation mostly done
    needs review and also some minor things

--- LakkTraceWPF/MainWindow.xaml.cs
+++ LakkTraceWPF/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace LakkTraceWPF
 {
@@ -46,10 +48,15 @@ namespace LakkTraceWPF
                 if (IsBothValid())
                 {
                     //time to call the database checking, because both field validated
-                    FormCleaner();
+                    SaveBtn_Click(sender, e);
+                }
+
+                if (Keyboard.FocusedElement == SaveBtn && !IsBothValid())
+                {
+                    FormErrorDisplay();
                 }
             }
-            
+
             if (Keyboard.FocusedElement == productTxbx && productTxbx.Text.Length > 0) //calls the validator for the field in focus
             {
                 ProductValidator();
@@ -58,7 +65,6 @@ namespace LakkTraceWPF
             if (Keyboard.FocusedElement == carrierTxbx && carrierTxbx.Text.Length > 0)
             {
                 CarrierValidator();
-                
             }
         }
 
@@ -74,7 +80,7 @@ namespace LakkTraceWPF
         {
             if (RegexValidation(productTxbx.Text, "productTxbx"))
             {
-                productLbl.Text = "Termék DataMatrix ellenőrizve!";
+                productLbl.Text = "Termék DM: " + productTxbx.Text;
                 IsProductValidated = true;
             }
             else
@@ -89,7 +95,7 @@ namespace LakkTraceWPF
         {
             if (RegexValidation(carrierTxbx.Text, "carrierTxbx"))
             {
-                carrierLbl.Text = "Keret DataMatrix ellenőrizve!";
+                carrierLbl.Text = "Keret DM: " + carrierTxbx.Text;
                 IsCarrierValidated = true;
             }
             else
@@ -119,7 +125,108 @@ namespace LakkTraceWPF
             productLbl.Text = "";
             carrierTxbx.Text = "";
             carrierLbl.Text = "";
+            dbresultLbl.Text = "";
+            IsProductValidated = false;
+            IsCarrierValidated = false;
             productTxbx.Focus();
+            this.mainStackPanel.Background = new SolidColorBrush(Colors.White);
+        }
+
+        //this is where the error flashing and communication happens
+        private void FormErrorDisplay()
+        {
+            productTxbx.Text = "";
+            carrierTxbx.Text = "";
+            IsProductValidated = false;
+            IsCarrierValidated = false;
+            productTxbx.Focus();
+            this.mainStackPanel.Background = new SolidColorBrush(Colors.Red);
+        }
+
+        //separates the products for database operations 
+        private string GetTableName()
+        {
+            if (carrierTxbx.Text.Contains("BMW"))
+                return "bmw";
+            else
+                return "volvo";
+        }
+
+        //have to know which side is in only proceed if its TOP then BOT
+        private string GetCarrierSide()
+        {
+            if (carrierTxbx.Text.Contains("TOP"))
+                return "TOP";
+            else
+                return "BOT";
+        }
+
+        private void DbValidation(string table)
+        {
+            string connstring = ConfigurationManager.ConnectionStrings["CCTrace.CCDBConnectionString"].ConnectionString;
+            // Making connection
+            var conn = new NpgsqlConnection(connstring);
+            conn.Open();
+            // building query
+            var cmd = new NpgsqlCommand("SELECT COUNT(*) FROM " + table + " WHERE prod_dm = :prod_dm", conn);
+            cmd.Parameters.Add(new NpgsqlParameter("prod_dm", productTxbx.Text));
+            Int32 countProd = Convert.ToInt32(cmd.ExecuteScalar());
+            conn.Close();
+
+            if (countProd == 0) // everything is fine the datamatrix is new in the database, proceed to upload
+            {
+                if (GetCarrierSide() == "TOP")
+                {
+                    DbInsert(table);
+                }
+                else
+                {
+                    dbresultLbl.Text = "TOP OLDALT LAKKOZD ELŐSZÖR!";
+                    FormErrorDisplay();
+                }
+            }
+            else if (countProd == 1) // the TOP side is coated, proceed only if this is the bottom
+            {
+                if (GetCarrierSide() == "BOT")
+                {
+                    DbInsert(table);
+                }
+                else
+                {
+                    dbresultLbl.Text = "ENNEK A TERMÉKNEK EZ AZ OLDALA MÁR LAKKOZVA VAN!";
+                    FormErrorDisplay();
+                }
+                
+            }
+            else // if its more than once in the database then the product's both side should be finished
+            {
+                dbresultLbl.Text = "EZ A TERMÉK MINDKÉT OLDALA LAKKOZOTT!";
+                FormErrorDisplay();
+            }
+        }
+
+        //insert values to database
+        private void DbInsert(string table) //DB insert
+        {
+            try
+            {
+                string connstring = ConfigurationManager.ConnectionStrings["CCTrace.CCDBConnectionString"].ConnectionString;
+                // Making connection with Npgsql provider
+                var conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                // building SQL query
+                var cmd = new NpgsqlCommand("INSERT INTO " + table + " (prod_dm, carr_dm, timestamp) VALUES(:prod_dm, :carr_dm, :timestamp)", conn);
+                cmd.Parameters.Add(new NpgsqlParameter("prod_dm", productTxbx.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("carr_dm", carrierTxbx.Text));
+                cmd.Parameters.Add(new NpgsqlParameter("timestamp", DateTime.Now));
+                cmd.ExecuteNonQuery();
+                //closing connection ASAP
+                conn.Close();
+            }
+            catch (Exception msg)
+            {
+                MessageBox.Show(msg.ToString());
+            }
         }
 
         //closes the app
@@ -128,14 +235,15 @@ namespace LakkTraceWPF
             this.Close();
         }
 
-        //savebutton for whatever, but the job should be done without clicking
+        //savebutton for collecting the final elements.
         private void SaveBtn_Click(object sender, RoutedEventArgs e)
         {
-            if (IsProductValidated)
-                MessageBox.Show("product valid");
-            if (IsCarrierValidated)
-                MessageBox.Show("carrier valid");
-            FormCleaner();
+            if (IsBothValid())
+            {
+                DbValidation(GetTableName());
+                FormCleaner();
+
+            }
         }
     }
 }
