73740b906086372358a8fbad682c25988025e95f
peltzergergely <peltzer.gergely@gmail.com>
2017-12-30 10:05:53 +0100

    display infos mostly done
    lakk showing and daily production too

--- LakkTraceWPF/MainWindow.xaml.cs
+++ LakkTraceWPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Npgsql;
 using System;
 using System.Configuration;
+using System.Data;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Input;
@@ -23,6 +24,8 @@ namespace LakkTraceWPF
             //make first textbox focused
             Loaded += (sender, e) => MoveFocus(new TraversalRequest(FocusNavigationDirection.Next));
             InitializeComponent();
+            DailyProduction();
+            VarnishDisplay();
         }
 
         //on Enter jumps to next focus, also triggers datavalidation
@@ -75,12 +78,12 @@ namespace LakkTraceWPF
             return (Regex.IsMatch(dataToValidate, rgx));
         }
 
-        //compares product with the regex rule
+        // Syntax check of the product
         private void ProductValidator()
         {
             if (RegexValidation(productTxbx.Text, "productTxbx"))
             {
-                productLbl.Text = "Termék DM: " + productTxbx.Text;
+                productLbl.Text = productTxbx.Text;
                 IsProductValidated = true;
             }
             else
@@ -90,12 +93,12 @@ namespace LakkTraceWPF
             }
         }
 
-        //compares carrier with regex rule
+        // Syntax check of the carrier 
         private void CarrierValidator()
         {
             if (RegexValidation(carrierTxbx.Text, "carrierTxbx"))
             {
-                carrierLbl.Text = "Keret DM: " + carrierTxbx.Text;
+                carrierLbl.Text = carrierTxbx.Text;
                 IsCarrierValidated = true;
             }
             else
@@ -105,7 +108,7 @@ namespace LakkTraceWPF
             }
         }
 
-        //checks if both fields are valid
+        // Check if both Syntax checked
         private bool IsBothValid()
         {
             if (IsProductValidated == true && IsCarrierValidated == true)
@@ -118,21 +121,18 @@ namespace LakkTraceWPF
             }
         }
 
-        //cleans the form for seamless data input
-        private void FormCleaner()
+        // Cleans the form for seamless data input
+        private void FormCleanerOnUploadFinished()
         {
             productTxbx.Text = "";
-            productLbl.Text = "";
             carrierTxbx.Text = "";
-            carrierLbl.Text = "";
-            dbresultLbl.Text = "";
             IsProductValidated = false;
             IsCarrierValidated = false;
             productTxbx.Focus();
             this.mainStackPanel.Background = new SolidColorBrush(Colors.White);
         }
 
-        //this is where the error flashing and communication happens
+        // Turns form red and keeps error message displayed on attempt to send wrong data
         private void FormErrorDisplay()
         {
             productTxbx.Text = "";
@@ -140,10 +140,11 @@ namespace LakkTraceWPF
             IsProductValidated = false;
             IsCarrierValidated = false;
             productTxbx.Focus();
+            this.dbresultLbl.Foreground = Brushes.Green;
             this.mainStackPanel.Background = new SolidColorBrush(Colors.Red);
         }
 
-        //separates the products for database operations 
+        // Gets product from carrier DM 
         private string GetTableName()
         {
             if (carrierTxbx.Text.Contains("BMW"))
@@ -152,7 +153,7 @@ namespace LakkTraceWPF
                 return "volvo";
         }
 
-        //have to know which side is in only proceed if its TOP then BOT
+        // Gets TOP/BOT from carrier DM
         private string GetCarrierSide()
         {
             if (carrierTxbx.Text.Contains("TOP"))
@@ -161,6 +162,7 @@ namespace LakkTraceWPF
                 return "BOT";
         }
 
+        // Validates before data upload
         private void DbValidation(string table)
         {
             string connstring = ConfigurationManager.ConnectionStrings["CCTrace.CCDBConnectionString"].ConnectionString;
@@ -173,7 +175,7 @@ namespace LakkTraceWPF
             Int32 countProd = Convert.ToInt32(cmd.ExecuteScalar());
             conn.Close();
 
-            if (countProd == 0) // everything is fine the datamatrix is new in the database, proceed to upload
+            if (countProd == 0) // If the product DM is not in the DB and is TOP side up then uploads
             {
                 if (GetCarrierSide() == "TOP")
                 {
@@ -185,7 +187,7 @@ namespace LakkTraceWPF
                     FormErrorDisplay();
                 }
             }
-            else if (countProd == 1) // the TOP side is coated, proceed only if this is the bottom
+            else if (countProd == 1) // Only allows BOT if it's already in the DB (see above)
             {
                 if (GetCarrierSide() == "BOT")
                 {
@@ -198,14 +200,14 @@ namespace LakkTraceWPF
                 }
                 
             }
-            else // if its more than once in the database then the product's both side should be finished
+            else // By this time product should be finished
             {
                 dbresultLbl.Text = "EZ A TERMÉK MINDKÉT OLDALA LAKKOZOTT!";
                 FormErrorDisplay();
             }
         }
 
-        //insert values to database
+        // Insert values to database
         private void DbInsert(string table) //DB insert
         {
             try
@@ -213,19 +215,81 @@ namespace LakkTraceWPF
                 string connstring = ConfigurationManager.ConnectionStrings["CCTrace.CCDBConnectionString"].ConnectionString;
                 // Making connection with Npgsql provider
                 var conn = new NpgsqlConnection(connstring);
+                var UploadMoment = DateTime.Now;
                 conn.Open();
                 // building SQL query
                 var cmd = new NpgsqlCommand("INSERT INTO " + table + " (prod_dm, carr_dm, timestamp) VALUES(:prod_dm, :carr_dm, :timestamp)", conn);
                 cmd.Parameters.Add(new NpgsqlParameter("prod_dm", productTxbx.Text));
                 cmd.Parameters.Add(new NpgsqlParameter("carr_dm", carrierTxbx.Text));
-                cmd.Parameters.Add(new NpgsqlParameter("timestamp", DateTime.Now));
+                cmd.Parameters.Add(new NpgsqlParameter("timestamp", UploadMoment));
                 cmd.ExecuteNonQuery();
                 //closing connection ASAP
                 conn.Close();
+                dbresultLbl.Text = UploadMoment.ToString("HH:mm:ss") + "-kor adatok mentve.";
+                FormCleanerOnUploadFinished();
             }
             catch (Exception msg)
             {
                 MessageBox.Show(msg.ToString());
+            }
+        }
+
+        // Shows the number of products uploaded this day
+        private void DailyProduction()
+        {
+            string connstring = ConfigurationManager.ConnectionStrings["CCTrace.CCDBConnectionString"].ConnectionString;
+            // Making connection
+            var conn = new NpgsqlConnection(connstring);
+            conn.Open();
+            // building query
+            var cmd = new NpgsqlCommand("SELECT COUNT(*) FROM bmw WHERE timestamp > 'today'", conn);
+            Int32 countbmw = Convert.ToInt32(cmd.ExecuteScalar());
+            cmd = new NpgsqlCommand("SELECT COUNT(*) FROM volvo WHERE timestamp > 'today'", conn);
+            Int32 countvolvo = Convert.ToInt32(cmd.ExecuteScalar());
+            conn.Close();
+            dailyProductionLbl.Text = "Mai napon lakkozva \r\n";
+            dailyProductionLbl.Text += "BMW db:";
+            dailyProductionLbl.Text += " " + countbmw.ToString();
+            dailyProductionLbl.Text += "\r\nVolvo db:";
+            dailyProductionLbl.Text += " " + countvolvo.ToString();
+            int sum = countvolvo + countbmw;
+            if (sum % 100 == 0)
+            {
+                MessageBox.Show("Ellenőrizd a lakk mennyiségét! Ha utántöltés szükséges akkor kattints a 'Lakk feltöltése gombra!' ");
+            }
+        }
+
+        // Shows the current varnish in a gridView
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+
+        private void VarnishDisplay()
+        {
+            try
+            {
+                // connstring stored in App.config
+                string connstring = ConfigurationManager.ConnectionStrings["CCTrace.CCDBConnectionString"].ConnectionString;
+                var conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                string sql = "SELECT batch, expdate, timestamp" +
+                    " FROM lakk order by timestamp desc limit 2";
+                // data adapter making request from our connection
+                var da = new NpgsqlDataAdapter(sql, conn);
+                ds.Reset();
+                // filling DataSet with result from NpgsqlDataAdapter
+                da.Fill(ds);
+                // since it C# DataSet can handle multiple tables, we will select first
+                dt = ds.Tables[0];
+                // connect grid to DataTable
+                dataGridView1.ItemsSource = dt.AsDataView();
+                // since we only showing the result we don't need connection anymore
+                conn.Close();
+            }
+            catch (Exception msg)
+            {
+                // error handling
+                MessageBox.Show(msg.ToString());
+                //throw;
             }
         }
 
@@ -241,9 +305,9 @@ namespace LakkTraceWPF
             if (IsBothValid())
             {
                 DbValidation(GetTableName());
-                FormCleaner();
-
             }
+            DailyProduction();
+            VarnishDisplay();
         }
     }
 }
